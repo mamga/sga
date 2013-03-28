e79594c091fad9da675a0e9e34a4ef3f8188f926
mamga <mamga05@gmail.com>
2013-03-28 17:30:57 -0300

    Etiqueta 1 OK - Teste

---
+++ src/gov/pr/seab/sga/dto/EtiquetaDTO.java
@@ -0,0 +1,404 @@
+package gov.pr.seab.sga.dto;
+
+public class EtiquetaDTO {
+	
+	private String orgaoEtiqueta1;
+	private String dataCadastroEtiqueta1;
+	private String numeroProtocoloEtiqueta1;
+	private String nomeInteressado1Etiqueta1;
+	private String nomeInteressado2Etiqueta1;
+	private String assuntoEtiqueta1;
+	private String municipioEtiqueta1;
+	private String origemEtiqueta1;
+	private String palavraChaveEtiqueta1;
+	private String documentoEtiqueta1;
+	private String assuntoComplementoEtiqueta1;
+	
+	private String orgaoEtiqueta2;
+	private String dataCadastroEtiqueta2;
+	private String numeroProtocoloEtiqueta2;
+	private String nomeInteressado1Etiqueta2;
+	private String nomeInteressado2Etiqueta2;
+	private String assuntoEtiqueta2;
+	private String municipioEtiqueta2;
+	private String origemEtiqueta2;
+	private String palavraChaveEtiqueta2;
+	private String documentoEtiqueta2;
+	private String assuntoComplementoEtiqueta2;
+	
+	private String orgaoEtiqueta3;
+	private String dataCadastroEtiqueta3;
+	private String numeroProtocoloEtiqueta3;
+	private String nomeInteressado1Etiqueta3;
+	private String nomeInteressado2Etiqueta3;
+	private String assuntoEtiqueta3;
+	private String municipioEtiqueta3;
+	private String origemEtiqueta3;
+	private String palavraChaveEtiqueta3;
+	private String documentoEtiqueta3;
+	private String assuntoComplementoEtiqueta3;
+	
+	private String orgaoEtiqueta4;
+	private String dataCadastroEtiqueta4;
+	private String numeroProtocoloEtiqueta4;
+	private String nomeInteressado1Etiqueta4;
+	private String nomeInteressado2Etiqueta4;
+	private String assuntoEtiqueta4;
+	private String municipioEtiqueta4;
+	private String origemEtiqueta4;
+	private String palavraChaveEtiqueta4;
+	private String documentoEtiqueta4;
+	private String assuntoComplementoEtiqueta4;
+	
+	private String orgaoEtiqueta5;
+	private String dataCadastroEtiqueta5;
+	private String numeroProtocoloEtiqueta5;
+	private String nomeInteressado1Etiqueta5;
+	private String nomeInteressado2Etiqueta5;
+	private String assuntoEtiqueta5;
+	private String municipioEtiqueta5;
+	private String origemEtiqueta5;
+	private String palavraChaveEtiqueta5;
+	private String documentoEtiqueta5;
+	private String assuntoComplementoEtiqueta5;
+	
+	
+	
+	
+	public String getOrgaoEtiqueta1() {
+		return orgaoEtiqueta1;
+	}
+	public void setOrgaoEtiqueta1(String orgaoEtiqueta1) {
+		this.orgaoEtiqueta1 = orgaoEtiqueta1;
+	}
+	public String getDataCadastroEtiqueta1() {
+		return dataCadastroEtiqueta1;
+	}
+	public void setDataCadastroEtiqueta1(String dataCadastroEtiqueta1) {
+		this.dataCadastroEtiqueta1 = dataCadastroEtiqueta1;
+	}
+	public String getNumeroProtocoloEtiqueta1() {
+		return numeroProtocoloEtiqueta1;
+	}
+	public void setNumeroProtocoloEtiqueta1(String numeroProtocoloEtiqueta1) {
+		this.numeroProtocoloEtiqueta1 = numeroProtocoloEtiqueta1;
+	}
+	public String getNomeInteressado1Etiqueta1() {
+		return nomeInteressado1Etiqueta1;
+	}
+	public void setNomeInteressado1Etiqueta1(String nomeInteressado1Etiqueta1) {
+		this.nomeInteressado1Etiqueta1 = nomeInteressado1Etiqueta1;
+	}
+	public String getNomeInteressado2Etiqueta1() {
+		return nomeInteressado2Etiqueta1;
+	}
+	public void setNomeInteressado2Etiqueta1(String nomeInteressado2Etiqueta1) {
+		this.nomeInteressado2Etiqueta1 = nomeInteressado2Etiqueta1;
+	}
+	public String getAssuntoEtiqueta1() {
+		return assuntoEtiqueta1;
+	}
+	public void setAssuntoEtiqueta1(String assuntoEtiqueta1) {
+		this.assuntoEtiqueta1 = assuntoEtiqueta1;
+	}
+	public String getMunicipioEtiqueta1() {
+		return municipioEtiqueta1;
+	}
+	public void setMunicipioEtiqueta1(String municipioEtiqueta1) {
+		this.municipioEtiqueta1 = municipioEtiqueta1;
+	}
+	public String getPalavraChaveEtiqueta1() {
+		return palavraChaveEtiqueta1;
+	}
+	public void setPalavraChaveEtiqueta1(String palavraChaveEtiqueta1) {
+		this.palavraChaveEtiqueta1 = palavraChaveEtiqueta1;
+	}
+	public String getDocumentoEtiqueta1() {
+		return documentoEtiqueta1;
+	}
+	public void setDocumentoEtiqueta1(String documentoEtiqueta1) {
+		this.documentoEtiqueta1 = documentoEtiqueta1;
+	}
+	public String getAssuntoComplementoEtiqueta1() {
+		return assuntoComplementoEtiqueta1;
+	}
+	public void setAssuntoComplementoEtiqueta1(String assuntoComplementoEtiqueta1) {
+		this.assuntoComplementoEtiqueta1 = assuntoComplementoEtiqueta1;
+	}
+	public String getOrgaoEtiqueta2() {
+		return orgaoEtiqueta2;
+	}
+	public void setOrgaoEtiqueta2(String orgaoEtiqueta2) {
+		this.orgaoEtiqueta2 = orgaoEtiqueta2;
+	}
+	public String getDataCadastroEtiqueta2() {
+		return dataCadastroEtiqueta2;
+	}
+	public void setDataCadastroEtiqueta2(String dataCadastroEtiqueta2) {
+		this.dataCadastroEtiqueta2 = dataCadastroEtiqueta2;
+	}
+	public String getNumeroProtocoloEtiqueta2() {
+		return numeroProtocoloEtiqueta2;
+	}
+	public void setNumeroProtocoloEtiqueta2(String numeroProtocoloEtiqueta2) {
+		this.numeroProtocoloEtiqueta2 = numeroProtocoloEtiqueta2;
+	}
+	public String getNomeInteressado1Etiqueta2() {
+		return nomeInteressado1Etiqueta2;
+	}
+	public void setNomeInteressado1Etiqueta2(String nomeInteressado1Etiqueta2) {
+		this.nomeInteressado1Etiqueta2 = nomeInteressado1Etiqueta2;
+	}
+	public String getNomeInteressado2Etiqueta2() {
+		return nomeInteressado2Etiqueta2;
+	}
+	public void setNomeInteressado2Etiqueta2(String nomeInteressado2Etiqueta2) {
+		this.nomeInteressado2Etiqueta2 = nomeInteressado2Etiqueta2;
+	}
+	public String getAssuntoEtiqueta2() {
+		return assuntoEtiqueta2;
+	}
+	public void setAssuntoEtiqueta2(String assuntoEtiqueta2) {
+		this.assuntoEtiqueta2 = assuntoEtiqueta2;
+	}
+	public String getMunicipioEtiqueta2() {
+		return municipioEtiqueta2;
+	}
+	public void setMunicipioEtiqueta2(String municipioEtiqueta2) {
+		this.municipioEtiqueta2 = municipioEtiqueta2;
+	}
+	public String getPalavraChaveEtiqueta2() {
+		return palavraChaveEtiqueta2;
+	}
+	public void setPalavraChaveEtiqueta2(String palavraChaveEtiqueta2) {
+		this.palavraChaveEtiqueta2 = palavraChaveEtiqueta2;
+	}
+	public String getDocumentoEtiqueta2() {
+		return documentoEtiqueta2;
+	}
+	public void setDocumentoEtiqueta2(String documentoEtiqueta2) {
+		this.documentoEtiqueta2 = documentoEtiqueta2;
+	}
+	public String getAssuntoComplementoEtiqueta2() {
+		return assuntoComplementoEtiqueta2;
+	}
+	public void setAssuntoComplementoEtiqueta2(String assuntoComplementoEtiqueta2) {
+		this.assuntoComplementoEtiqueta2 = assuntoComplementoEtiqueta2;
+	}
+	public String getOrgaoEtiqueta3() {
+		return orgaoEtiqueta3;
+	}
+	public void setOrgaoEtiqueta3(String orgaoEtiqueta3) {
+		this.orgaoEtiqueta3 = orgaoEtiqueta3;
+	}
+	public String getDataCadastroEtiqueta3() {
+		return dataCadastroEtiqueta3;
+	}
+	public void setDataCadastroEtiqueta3(String dataCadastroEtiqueta3) {
+		this.dataCadastroEtiqueta3 = dataCadastroEtiqueta3;
+	}
+	public String getNumeroProtocoloEtiqueta3() {
+		return numeroProtocoloEtiqueta3;
+	}
+	public void setNumeroProtocoloEtiqueta3(String numeroProtocoloEtiqueta3) {
+		this.numeroProtocoloEtiqueta3 = numeroProtocoloEtiqueta3;
+	}
+	public String getNomeInteressado1Etiqueta3() {
+		return nomeInteressado1Etiqueta3;
+	}
+	public void setNomeInteressado1Etiqueta3(String nomeInteressado1Etiqueta3) {
+		this.nomeInteressado1Etiqueta3 = nomeInteressado1Etiqueta3;
+	}
+	public String getNomeInteressado2Etiqueta3() {
+		return nomeInteressado2Etiqueta3;
+	}
+	public void setNomeInteressado2Etiqueta3(String nomeInteressado2Etiqueta3) {
+		this.nomeInteressado2Etiqueta3 = nomeInteressado2Etiqueta3;
+	}
+	public String getAssuntoEtiqueta3() {
+		return assuntoEtiqueta3;
+	}
+	public void setAssuntoEtiqueta3(String assuntoEtiqueta3) {
+		this.assuntoEtiqueta3 = assuntoEtiqueta3;
+	}
+	public String getMunicipioEtiqueta3() {
+		return municipioEtiqueta3;
+	}
+	public void setMunicipioEtiqueta3(String municipioEtiqueta3) {
+		this.municipioEtiqueta3 = municipioEtiqueta3;
+	}
+	public String getPalavraChaveEtiqueta3() {
+		return palavraChaveEtiqueta3;
+	}
+	public void setPalavraChaveEtiqueta3(String palavraChaveEtiqueta3) {
+		this.palavraChaveEtiqueta3 = palavraChaveEtiqueta3;
+	}
+	public String getDocumentoEtiqueta3() {
+		return documentoEtiqueta3;
+	}
+	public void setDocumentoEtiqueta3(String documentoEtiqueta3) {
+		this.documentoEtiqueta3 = documentoEtiqueta3;
+	}
+	public String getAssuntoComplementoEtiqueta3() {
+		return assuntoComplementoEtiqueta3;
+	}
+	public void setAssuntoComplementoEtiqueta3(String assuntoComplementoEtiqueta3) {
+		this.assuntoComplementoEtiqueta3 = assuntoComplementoEtiqueta3;
+	}
+	public String getOrgaoEtiqueta4() {
+		return orgaoEtiqueta4;
+	}
+	public void setOrgaoEtiqueta4(String orgaoEtiqueta4) {
+		this.orgaoEtiqueta4 = orgaoEtiqueta4;
+	}
+	public String getDataCadastroEtiqueta4() {
+		return dataCadastroEtiqueta4;
+	}
+	public void setDataCadastroEtiqueta4(String dataCadastroEtiqueta4) {
+		this.dataCadastroEtiqueta4 = dataCadastroEtiqueta4;
+	}
+	public String getNumeroProtocoloEtiqueta4() {
+		return numeroProtocoloEtiqueta4;
+	}
+	public void setNumeroProtocoloEtiqueta4(String numeroProtocoloEtiqueta4) {
+		this.numeroProtocoloEtiqueta4 = numeroProtocoloEtiqueta4;
+	}
+	public String getNomeInteressado1Etiqueta4() {
+		return nomeInteressado1Etiqueta4;
+	}
+	public void setNomeInteressado1Etiqueta4(String nomeInteressado1Etiqueta4) {
+		this.nomeInteressado1Etiqueta4 = nomeInteressado1Etiqueta4;
+	}
+	public String getNomeInteressado2Etiqueta4() {
+		return nomeInteressado2Etiqueta4;
+	}
+	public void setNomeInteressado2Etiqueta4(String nomeInteressado2Etiqueta4) {
+		this.nomeInteressado2Etiqueta4 = nomeInteressado2Etiqueta4;
+	}
+	public String getAssuntoEtiqueta4() {
+		return assuntoEtiqueta4;
+	}
+	public void setAssuntoEtiqueta4(String assuntoEtiqueta4) {
+		this.assuntoEtiqueta4 = assuntoEtiqueta4;
+	}
+	public String getMunicipioEtiqueta4() {
+		return municipioEtiqueta4;
+	}
+	public void setMunicipioEtiqueta4(String municipioEtiqueta4) {
+		this.municipioEtiqueta4 = municipioEtiqueta4;
+	}
+	public String getPalavraChaveEtiqueta4() {
+		return palavraChaveEtiqueta4;
+	}
+	public void setPalavraChaveEtiqueta4(String palavraChaveEtiqueta4) {
+		this.palavraChaveEtiqueta4 = palavraChaveEtiqueta4;
+	}
+	public String getDocumentoEtiqueta4() {
+		return documentoEtiqueta4;
+	}
+	public void setDocumentoEtiqueta4(String documentoEtiqueta4) {
+		this.documentoEtiqueta4 = documentoEtiqueta4;
+	}
+	public String getAssuntoComplementoEtiqueta4() {
+		return assuntoComplementoEtiqueta4;
+	}
+	public void setAssuntoComplementoEtiqueta4(String assuntoComplementoEtiqueta4) {
+		this.assuntoComplementoEtiqueta4 = assuntoComplementoEtiqueta4;
+	}
+	public String getOrgaoEtiqueta5() {
+		return orgaoEtiqueta5;
+	}
+	public void setOrgaoEtiqueta5(String orgaoEtiqueta5) {
+		this.orgaoEtiqueta5 = orgaoEtiqueta5;
+	}
+	public String getDataCadastroEtiqueta5() {
+		return dataCadastroEtiqueta5;
+	}
+	public void setDataCadastroEtiqueta5(String dataCadastroEtiqueta5) {
+		this.dataCadastroEtiqueta5 = dataCadastroEtiqueta5;
+	}
+	public String getNumeroProtocoloEtiqueta5() {
+		return numeroProtocoloEtiqueta5;
+	}
+	public void setNumeroProtocoloEtiqueta5(String numeroProtocoloEtiqueta5) {
+		this.numeroProtocoloEtiqueta5 = numeroProtocoloEtiqueta5;
+	}
+	public String getNomeInteressado1Etiqueta5() {
+		return nomeInteressado1Etiqueta5;
+	}
+	public void setNomeInteressado1Etiqueta5(String nomeInteressado1Etiqueta5) {
+		this.nomeInteressado1Etiqueta5 = nomeInteressado1Etiqueta5;
+	}
+	public String getNomeInteressado2Etiqueta5() {
+		return nomeInteressado2Etiqueta5;
+	}
+	public void setNomeInteressado2Etiqueta5(String nomeInteressado2Etiqueta5) {
+		this.nomeInteressado2Etiqueta5 = nomeInteressado2Etiqueta5;
+	}
+	public String getAssuntoEtiqueta5() {
+		return assuntoEtiqueta5;
+	}
+	public void setAssuntoEtiqueta5(String assuntoEtiqueta5) {
+		this.assuntoEtiqueta5 = assuntoEtiqueta5;
+	}
+	public String getMunicipioEtiqueta5() {
+		return municipioEtiqueta5;
+	}
+	public void setMunicipioEtiqueta5(String municipioEtiqueta5) {
+		this.municipioEtiqueta5 = municipioEtiqueta5;
+	}
+	public String getPalavraChaveEtiqueta5() {
+		return palavraChaveEtiqueta5;
+	}
+	public void setPalavraChaveEtiqueta5(String palavraChaveEtiqueta5) {
+		this.palavraChaveEtiqueta5 = palavraChaveEtiqueta5;
+	}
+	public String getDocumentoEtiqueta5() {
+		return documentoEtiqueta5;
+	}
+	public void setDocumentoEtiqueta5(String documentoEtiqueta5) {
+		this.documentoEtiqueta5 = documentoEtiqueta5;
+	}
+	public String getAssuntoComplementoEtiqueta5() {
+		return assuntoComplementoEtiqueta5;
+	}
+	public void setAssuntoComplementoEtiqueta5(String assuntoComplementoEtiqueta5) {
+		this.assuntoComplementoEtiqueta5 = assuntoComplementoEtiqueta5;
+	}
+	public String getOrigemEtiqueta1() {
+		return origemEtiqueta1;
+	}
+	public void setOrigemEtiqueta1(String origemEtiqueta1) {
+		this.origemEtiqueta1 = origemEtiqueta1;
+	}
+	public String getOrigemEtiqueta2() {
+		return origemEtiqueta2;
+	}
+	public void setOrigemEtiqueta2(String origemEtiqueta2) {
+		this.origemEtiqueta2 = origemEtiqueta2;
+	}
+	public String getOrigemEtiqueta3() {
+		return origemEtiqueta3;
+	}
+	public void setOrigemEtiqueta3(String origemEtiqueta3) {
+		this.origemEtiqueta3 = origemEtiqueta3;
+	}
+	public String getOrigemEtiqueta4() {
+		return origemEtiqueta4;
+	}
+	public void setOrigemEtiqueta4(String origemEtiqueta4) {
+		this.origemEtiqueta4 = origemEtiqueta4;
+	}
+	public String getOrigemEtiqueta5() {
+		return origemEtiqueta5;
+	}
+	public void setOrigemEtiqueta5(String origemEtiqueta5) {
+		this.origemEtiqueta5 = origemEtiqueta5;
+	}
+	
+	
+	
+	
+	
+
+}
--- src/gov/pr/seab/sga/managedbean/EtiquetaManagedBean.java
+++ src/gov/pr/seab/sga/managedbean/EtiquetaManagedBean.java
@@ -1,9 +1,11 @@
 package gov.pr.seab.sga.managedbean;
 
-import gov.pr.seab.sga.bean.Setor;
+import gov.pr.seab.sga.bean.Protocolo;
+import gov.pr.seab.sga.dto.EtiquetaDTO;
+import gov.pr.seab.sga.dto.ProtocoloDTO;
+import gov.pr.seab.sga.facade.ProtocoloFacade;
 import gov.pr.seab.sga.util.Relatorio;
 
-import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -16,7 +18,6 @@ import javax.faces.event.ActionEvent;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 
-import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
@@ -29,43 +30,42 @@ public class EtiquetaManagedBean extends AbstractManagedBean implements Serializ
 	
 	private static final long serialVersionUID = 1L;
 	
-	private String numeroProtocoloEtiquetaPos1;
-	private String numeroProtocoloEtiquetaPos2;
-	private String numeroProtocoloEtiquetaPos3;
-	private String numeroProtocoloEtiquetaPos4;
-	private String numeroProtocoloEtiquetaPos5;
+	private String numeroProtocoloEtiqueta1;
+	private String numeroProtocoloEtiqueta2;
+	private String numeroProtocoloEtiqueta3;
+	private String numeroProtocoloEtiqueta4;
+	private String numeroProtocoloEtiqueta5;
 	
- 
 	
-	public String getNumeroProtocoloEtiquetaPos1() {
-		return numeroProtocoloEtiquetaPos1;
+	public String getNumeroProtocoloEtiqueta1() {
+		return numeroProtocoloEtiqueta1;
 	}
-	public void setNumeroProtocoloEtiquetaPos1(String numeroProtocoloEtiquetaPos1) {
-		this.numeroProtocoloEtiquetaPos1 = numeroProtocoloEtiquetaPos1;
+	public void setNumeroProtocoloEtiqueta1(String numeroProtocoloEtiqueta1) {
+		this.numeroProtocoloEtiqueta1 = numeroProtocoloEtiqueta1;
 	}
-	public String getNumeroProtocoloEtiquetaPos2() {
-		return numeroProtocoloEtiquetaPos2;
+	public String getNumeroProtocoloEtiqueta2() {
+		return numeroProtocoloEtiqueta2;
 	}
-	public void setNumeroProtocoloEtiquetaPos2(String numeroProtocoloEtiquetaPos2) {
-		this.numeroProtocoloEtiquetaPos2 = numeroProtocoloEtiquetaPos2;
+	public void setNumeroProtocoloEtiqueta2(String numeroProtocoloEtiqueta2) {
+		this.numeroProtocoloEtiqueta2 = numeroProtocoloEtiqueta2;
 	}
-	public String getNumeroProtocoloEtiquetaPos3() {
-		return numeroProtocoloEtiquetaPos3;
+	public String getNumeroProtocoloEtiqueta3() {
+		return numeroProtocoloEtiqueta3;
 	}
-	public void setNumeroProtocoloEtiquetaPos3(String numeroProtocoloEtiquetaPos3) {
-		this.numeroProtocoloEtiquetaPos3 = numeroProtocoloEtiquetaPos3;
+	public void setNumeroProtocoloEtiqueta3(String numeroProtocoloEtiqueta3) {
+		this.numeroProtocoloEtiqueta3 = numeroProtocoloEtiqueta3;
 	}
-	public String getNumeroProtocoloEtiquetaPos4() {
-		return numeroProtocoloEtiquetaPos4;
+	public String getNumeroProtocoloEtiqueta4() {
+		return numeroProtocoloEtiqueta4;
 	}
-	public void setNumeroProtocoloEtiquetaPos4(String numeroProtocoloEtiquetaPos4) {
-		this.numeroProtocoloEtiquetaPos4 = numeroProtocoloEtiquetaPos4;
+	public void setNumeroProtocoloEtiqueta4(String numeroProtocoloEtiqueta4) {
+		this.numeroProtocoloEtiqueta4 = numeroProtocoloEtiqueta4;
 	}
-	public String getNumeroProtocoloEtiquetaPos5() {
-		return numeroProtocoloEtiquetaPos5;
+	public String getNumeroProtocoloEtiqueta5() {
+		return numeroProtocoloEtiqueta5;
 	}
-	public void setNumeroProtocoloEtiquetaPos5(String numeroProtocoloEtiquetaPos5) {
-		this.numeroProtocoloEtiquetaPos5 = numeroProtocoloEtiquetaPos5;
+	public void setNumeroProtocoloEtiqueta5(String numeroProtocoloEtiqueta5) {
+		this.numeroProtocoloEtiqueta5 = numeroProtocoloEtiqueta5;
 	}
 	
 	public void gerarArquivoImpressao (ActionEvent actionEvent) {
@@ -77,25 +77,76 @@ public class EtiquetaManagedBean extends AbstractManagedBean implements Serializ
 	
 	
 
-	public void gerarRelatorioTeste () throws IOException, JRException {
+	public void gerarRelatorioTeste () throws Exception {
 		
-        List<Setor> listaSetor = new ArrayList<Setor>();
-        Setor setor = new Setor();
-        setor.setDescricao("Informática");
-        setor.setSigla("NII");
-        
-        listaSetor.add(setor);
-        
-                
-        JRBeanCollectionDataSource beanCollectionDataSource = new JRBeanCollectionDataSource(listaSetor);
-        String reportPath = FacesContext.getCurrentInstance().getExternalContext().getRealPath("/relatorios/etiqueta.jasper");
-        JasperPrint jasperPrint = JasperFillManager.fillReport(reportPath, new HashMap(), beanCollectionDataSource);
-        HttpServletResponse httpServletResponse = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
-        httpServletResponse.addHeader("Content-disposition", "attachment; filename=etiqueta.pdf");
-        ServletOutputStream servletOutputStream = httpServletResponse.getOutputStream();
-        JasperExportManager.exportReportToPdfStream(jasperPrint, servletOutputStream);
-        FacesContext.getCurrentInstance().responseComplete();
-		
+		try {
+	        List<EtiquetaDTO> listaEtiqueta = new ArrayList<EtiquetaDTO>();
+	        Protocolo protocolo = null;
+	        ProtocoloDTO protocoloDTO = null;
+	        EtiquetaDTO etiqueta = null;
+	        
+	        if ((numeroProtocoloEtiqueta1 != null) || (numeroProtocoloEtiqueta1 != "")) {
+	        	protocoloDTO = new ProtocoloDTO();
+	        	protocoloDTO.setNumeroProtocolo(numeroProtocoloEtiqueta1);
+	        	
+	        	//protocolo = ProtocoloFacade.obterProtocolo(protocoloDTO);
+	        	
+	        	protocolo = new Protocolo();
+	        	protocolo.setDataCadastro("01/01/1950 10:50");
+	        	protocolo.setNumeroProtocolo("11.399.905-5");
+	        	protocolo.setInteressadoNome1("Nome Interessado 1");
+	        	protocolo.setInteressadoNome2("Nome Interessado 2");
+	        	protocolo.setAssunto("Assunto Etiqueta 1");
+	        	protocolo.setCidade("Curitiba");
+	        	protocolo.setOrigem("Prefeitura");
+	        	protocolo.setPalavra1("Palavra Chave 1");
+	        	protocolo.setPalavra2("Palavra Chave 2");
+	        	protocolo.setNumeroDocumento("009");
+	        	protocolo.setAnoDocumento("2013");
+	        	
+	        	
+	        	
+	        	if (protocolo != null) {
+	        		etiqueta = new EtiquetaDTO();
+	        		etiqueta.setOrgaoEtiqueta1("<b>Órgão:</b>" + " SEAB");
+	        		etiqueta.setDataCadastroEtiqueta1("<b>Em:</b> " + protocolo.getDataCadastro());
+	        		etiqueta.setNumeroProtocoloEtiqueta1(protocolo.getNumeroProtocolo());
+	        		etiqueta.setNomeInteressado1Etiqueta1("<b>Interessado 1:</b> " + protocolo.getInteressadoNome1());
+	        		etiqueta.setNomeInteressado2Etiqueta1("<b>Interessado 2:</b> " + protocolo.getInteressadoNome2());
+	        		etiqueta.setAssuntoEtiqueta1("<b>Assunto:</b> " + protocolo.getAssunto());
+	        		etiqueta.setMunicipioEtiqueta1("<b>Cidade:</b> " + protocolo.getCidade());
+	        		etiqueta.setOrigemEtiqueta1("<b>Origem:</b> " + protocolo.getOrigem());
+	        		etiqueta.setPalavraChaveEtiqueta1("<b>Palavras Chaves:</b> " + protocolo.getPalavra1() + " / " + protocolo.getPalavra2());
+	        		etiqueta.setDocumentoEtiqueta1("<b>Número/Ano Documento:</b> " + protocolo.getNumeroDocumento()+ "/" + protocolo.getAnoDocumento());
+	        		etiqueta.setAssuntoComplementoEtiqueta1("<b>Assunto/Complemento:</b> " + "??????????????");
+	        		
+	        		listaEtiqueta.add(etiqueta);
+	        		
+	        	} else {
+	        		displayInfoMessageToUsuario("- Protocolo " + numeroProtocoloEtiqueta1 + " não encontrado." );
+	        	}
+	        	
+	        	
+	        }
+	        
+	        
+	        
+	        if (!listaEtiqueta.isEmpty() ) {
+		        JRBeanCollectionDataSource beanCollectionDataSource = new JRBeanCollectionDataSource(listaEtiqueta);
+		        String reportPath = FacesContext.getCurrentInstance().getExternalContext().getRealPath("/relatorios/etiquetaa4.jasper");
+		        JasperPrint jasperPrint = JasperFillManager.fillReport(reportPath, new HashMap(), beanCollectionDataSource);
+		        HttpServletResponse httpServletResponse = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
+		        httpServletResponse.addHeader("Content-disposition", "attachment; filename=etiqueta.pdf");
+		        ServletOutputStream servletOutputStream = httpServletResponse.getOutputStream();
+		        JasperExportManager.exportReportToPdfStream(jasperPrint, servletOutputStream);
+		        FacesContext.getCurrentInstance().responseComplete();
+	        } else {
+	        	displayInfoMessageToUsuario("- Nenhum protocolo informado foi encontrado." );
+	        }
+	        
+		} catch (Exception e) {
+			displayInfoMessageToUsuario("- Erro não tratado: " + e );
+		}
 	}
 	
 
