893a1636a7ac4cae9a67d97d51e4832c56037dbd
mamga <mamga05@gmail.com>
2013-04-07 19:58:22 -0300

    E-mail comentado

--- src/gov/pr/seab/sga/managedbean/MenuManagedBean.java
+++ src/gov/pr/seab/sga/managedbean/MenuManagedBean.java
@@ -38,7 +38,7 @@ public class MenuManagedBean {
         model.addSubmenu(submenu);
           
         //Second submenu  
-        submenu = new Submenu();  
+        /*submenu = new Submenu();  
         submenu.setLabel("Dynamic Submenu 2");  
           
         item = new MenuItem();  
@@ -51,7 +51,7 @@ public class MenuManagedBean {
         item.setUrl("#");  
         submenu.getChildren().add(item);  
           
-        model.addSubmenu(submenu);
+        model.addSubmenu(submenu);*/
 			
 	}
 	
@@ -82,3 +82,35 @@ public class MenuManagedBean {
 	
 	
 }
+
+
+
+
+/*Prezados(as),
+
+Para facilitar o processo de implantação do Sistema AAX nos núcleos regionais da SEAB, foi desenvolvido um módulo, dentro do sistema de acompanhamento de processos que está em construção pelo NII, para impressão das etiquetas.
+
+Após o cadastro no AAX, seguir as intruções abaixo para impressão das etiquetas:
+
+- Acessar o endereço: http://10.36.1.13/sga/
+- Menu "Sistema AAX -> Impressão de Etiquetas".
+- Digitar o número do protocolo no campo correspondente a posição da etiqueta.
+- Clicar no botão "Gerar Arquivo para Impressão".
+- Imprimir o PDF.
+
+Na folha, a etiqueta 1 é a que está mais próximo da margem maior.
+
+Qualquer dúvida estamos à disposição nos seguintes telefones:
+
+41 3313 4164
+41 3313 4057
+
+Att.
+
+Marcelo Andrade Amadeu
+Secretaria da Agricultura e do Abastecimento - SEAB
+Núcleo de Informática e Informações - NII
+
+Esta mensagem pode conter informações   confidenciais e/ou privilegiadas.Se você não for o destinatário ou a  pessoa autorizada a recebe-la, não pode usar, copiar ou divulgar as  informações nela contidas ou tomar qualquer ação baseada nelas. Se  você recebeu esta mensagem por engano,por favor, avise imediatamente o  remetente, e em seguida, apague-a. Comunicações pela Internet não  podem ser garantidas quanto à segurança ou inexistência de erros ou  de vírus. O remetente, por esta razão, não aceita responsabilidade  por qualquer erro ou omissão no contexto da mensagem decorrente da  transmissão via Internet.
+
+*/
